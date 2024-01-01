ef46b643ab016b9df006dc1d66d7706f88fa0fab
buzhfianji <buzhifanji@163.com>
2024-01-01 23:54:51 +0800

    添加当前活动路由样式

--- src/routes/list/+layout.svelte
+++ src/routes/list/+layout.svelte
@@ -9,37 +9,39 @@
 	const articlePath = '/list/article-list';
 	const settingPath = '/list/setting';
 	const tooltipClass = 'tooltip tooltip-right tooltip-accent';
+
+	$: fullPath = $page.url.pathname;
+	const isActive = (path: string, currentPath: string) =>
+		path === currentPath ? 'active ' + tooltipClass : tooltipClass;
 </script>
 
 <main class="h-screen w-full flex">
-	<aside class="flex flex-col justify-between bg-base-200">
-		<nav>
-			<ul class="menu rounded-box gap-4">
-				<!-- <li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
+	<nav class="flex flex-col justify-between bg-base-200">
+		<ul class="menu rounded-box gap-4">
+			<!-- <li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
 					<a href="/">首页</a>
 				</li> -->
-				<li aria-current={getPathName($page.url.pathname, bookPath)}>
-					<a href={bookPath} class={tooltipClass} data-tip={$t('lang.book')}>
-						<BookOpen />
-					</a>
-				</li>
-				<li aria-current={getPathName($page.url.pathname, articlePath)}>
-					<a href={articlePath} class={tooltipClass} data-tip={$t('lang.article')}>
-						<Newspaper />
-					</a>
-				</li>
-			</ul>
-		</nav>
+			<li aria-current={getPathName(fullPath, bookPath)}>
+				<a href={bookPath} class={isActive(fullPath, bookPath)} data-tip={$t('lang.book')}>
+					<BookOpen />
+				</a>
+			</li>
+			<li aria-current={getPathName(fullPath, articlePath)}>
+				<a href={articlePath} class={isActive(fullPath, articlePath)} data-tip={$t('lang.article')}>
+					<Newspaper />
+				</a>
+			</li>
+		</ul>
 		<ul class="menu rounded-box gap-4">
-			<li aria-current={getPathName($page.url.pathname, settingPath)}>
-				<a href={settingPath} class={tooltipClass} data-tip={$t('lang.setting')}>
+			<li aria-current={getPathName(fullPath, settingPath)}>
+				<a href={settingPath} class={isActive(fullPath, settingPath)} data-tip={$t('lang.setting')}>
 					<Settings />
 				</a>
 			</li>
 		</ul>
-	</aside>
+	</nav>
 
-	<section class="flex flex-1">
+	<section class="flex flex-1 flex-row">
 		<RouterTransition>
 			<slot />
 		</RouterTransition>
